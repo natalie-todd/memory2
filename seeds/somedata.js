189ef31eeac08813e89781eeafac6c89e0842c79
Natalie <natalie.todd15@gmail.com>
2018-08-05 23:05:08 -0600

    now seeded

--- seeds/somedata.js
+++ seeds/somedata.js
@@ -1,15 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('coffee').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('coffee').insert([
         {id: 1, name: 'Black and Tan', roaster: 'Ink', aroma: 3},
         {id: 2, name: 'Holiday Roast', roaster: 'Starbucks', aroma: 9},
         {id: 3, name: 'House Quake', roaster: 'Denver Coffee', aroma: 6}
       ]);
     }).then(() => {
-      return knex.raw('ALTER SEQUENCE resolution_id_seq RESTART WITH 4;')
+      return knex.raw('ALTER SEQUENCE coffee_id_seq RESTART WITH 4;')
     });
 };
